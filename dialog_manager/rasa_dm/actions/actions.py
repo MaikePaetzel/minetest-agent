242a162c7b40ade52f37a5fd1e0da0b9207952ab
yemyemyem <meryem.karalioglu@web.de>
2021-06-16 17:35:13 +0200

    add rasa dialogue manager

---
+++ dialog_manager/rasa_dm/actions/actions.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class BotBrain:
+    # dummy class
+    def send_action(self, action):
+        print(f"bot brain received action {action}")
+
+
+class BotInstruction:
+    pass
+
+
+@dataclass
+class MoveBlock(BotInstruction):
+    which_block: str
+    where_to: str
+
+
+@dataclass
+class ComeHere(BotInstruction):
+    where_to: str
+
+
+
+@dataclass
+class Stop(BotInstruction):
+    pass
+
+
+
+class ActionSendBotBrain(Action):
+    def __init__(self):
+        super().__init__()
+        self.bot_brain = BotBrain()
+
+    def name(self) -> Text:
+        return "action_send_bot_brain"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        
+        if tracker.get_intent_of_latest_message() == "ask_move_block":
+        
+            bot_action = MoveBlock(
+                tracker.get_slot("which_block"),
+                tracker.get_slot("position")
+            )
+            dispatcher.utter_message(text="I will move the block")
+            self.bot_brain.send_action(bot_action)
+        
+        if tracker.get_intent_of_latest_message() == "ask_come_here":
+            bot_action = ComeHere(
+                tracker.get_slot("position")
+            )
+            dispatcher.utter_message(text="Okay, I'll come to where you are.")
+            self.bot_brain.send_action(bot_action)
+            
+
+        if tracker.get_intent_of_latest_message() == "ask_bot_stop_action":
+            dispatcher.utter_message(text="I am stopping")
+            self.bot_brain.send_action(Stop())
+
+        return []
+
+
